ba7e6719b3f7f84182ef381c5674b2968e408d0e
Jacob Shafton <jacob@shafton.com>
2022-01-07 08:23:48 -0700

    fix colorscheme loading

--- lua/plugins/sonokai.lua
+++ lua/plugins/sonokai.lua
@@ -2,3 +2,6 @@ vim.g.sonokai_better_performance = 1
 vim.g.sonokai_style = 'andromeda'
 vim.g.sonokai_enable_italic = 1
 vim.g.sonokai_disable_italic_comment = 0
+
+vim.opt.background = 'dark'
+vim.cmd [[colorscheme sonokai]]
