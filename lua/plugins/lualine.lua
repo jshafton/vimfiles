b12fc113b14978ee8520d9142a4612cdfd5fe6fa
Jacob Shafton <jacob@shafton.com>
2022-02-17 10:45:40 -0700

    clean up lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -30,7 +30,7 @@ require'lualine'.setup {
         },
       }
     },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'filetype'},
     lualine_y = {'progress'},
     -- lualine_z = {'location'}
     lualine_z = {
@@ -40,11 +40,26 @@ require'lualine'.setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,      -- Displays file status (readonly status, modified status)
+        path = 1,                -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+
+        shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+        symbols = {
+          modified = '[+]',      -- Text to show when the file is modified.
+          readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+        },
+      }
+    },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  extensions = {}
+  extensions = { 'fugitive', 'nvim-tree' }
 }
