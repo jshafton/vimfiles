578b6cc7966d0fcac0c5df4973c1195fcc74efb5
Jacob Shafton <jacob@shafton.com>
2022-04-06 08:35:53 -0600

    update nvim-tree config for breaking changes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -14,11 +14,9 @@
 --- See: `help NvimTree`
 local g = vim.g
 
-g.nvim_tree_quit_on_open = 0
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_respect_buf_cwd = 1
 g.nvim_tree_width_allow_resize  = 1
 g.nvim_tree_show_icons = {
@@ -48,4 +46,12 @@ require('nvim-tree').setup {
     enable = true,
     ignore = true,
   },
+  actions = {
+    quit_on_open = false,
+    open_file = {
+      window_picker = {
+        enable = false
+      }
+    }
+  }
 }
