e203f028c262c28dfaab33333788016ceee4e438
Jacob Shafton <jacob@shafton.com>
2023-08-18 13:15:44 -0600

    update neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
+  branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -16,7 +17,9 @@ return {
       -- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/v2.x/lua/neo-tree/defaults.lua
       close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
       filesystem = {
-        follow_current_file = true
+        follow_current_file = {
+          enabled = true
+        }
       }
     })
   end
