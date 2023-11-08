f8126877cf052c3d89d222672cca71303da9c9b0
Jacob Shafton <jacob@shafton.com>
2023-11-08 14:17:57 -0700

    further updates

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,15 +1,21 @@
 return {
-  'mfussenegger/nvim-lint',
-  event = { "BufWritePost" },
-  config = function ()
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
+	"mfussenegger/nvim-lint",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"rshkarin/mason-nvim-lint",
+	},
+	event = { "BufWritePost" },
+	config = function()
+		require("mason-nvim-lint").setup()
 
-    require('lint').linters_by_ft = {
-      json = {'jsonlint',}
-    }
-  end
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+
+		require("lint").linters_by_ft = {
+			json = { "jsonlint" },
+		}
+	end,
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,208 +1,196 @@
 return {
-  {
-    "szw/vim-maximizer",
-    keys = {
-      { "<leader>f", "<cmd>MaximizerToggle<CR>",   "n" },
-      { "<leader>f", "<cmd>MaximizerToggle<CR>gv", "v" },
-    },
-    config = function()
-      vim.g.maximizer_set_default_mapping = 0
-    end,
-  },
+	{
+		"szw/vim-maximizer",
+		keys = {
+			{ "<leader>f", "<cmd>MaximizerToggle<CR>", "n" },
+			{ "<leader>f", "<cmd>MaximizerToggle<CR>gv", "v" },
+		},
+		config = function()
+			vim.g.maximizer_set_default_mapping = 0
+		end,
+	},
 
-  {
-    "numToStr/Navigator.nvim",
-    keys = {
-      { "<C-h>", "<cmd>NavigatorLeft<CR>",  "n" },
-      { "<C-l>", "<cmd>NavigatorRight<CR>", "n" },
-      { "<C-k>", "<cmd>NavigatorUp<CR>",    "n" },
-      { "<C-j>", "<cmd>NavigatorDown<CR>",  "n" },
-    },
-    config = true,
-  },
+	{
+		"numToStr/Navigator.nvim",
+		keys = {
+			{ "<C-h>", "<cmd>NavigatorLeft<CR>", "n" },
+			{ "<C-l>", "<cmd>NavigatorRight<CR>", "n" },
+			{ "<C-k>", "<cmd>NavigatorUp<CR>", "n" },
+			{ "<C-j>", "<cmd>NavigatorDown<CR>", "n" },
+		},
+		config = true,
+	},
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {
-    },
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
 
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        -- component_separators = { left = '', right = ''},
-        -- section_separators = { left = '', right = ''},
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
-        disabled_filetypes = {},
-        always_divide_middle = false,
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          {
-            "filename",
-            file_status = true, -- Displays file status (readonly status, modified status)
-            path = 1,           -- 0: Just the filename
-            -- 1: Relative path
-            -- 2: Absolute path
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				-- component_separators = { left = '', right = ''},
+				-- section_separators = { left = '', right = ''},
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				disabled_filetypes = {},
+				always_divide_middle = false,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- Displays file status (readonly status, modified status)
+						path = 1, -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
 
-            shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-            symbols = {
-              modified = "[+]",      -- Text to show when the file is modified.
-              readonly = "[-]",      -- Text to show when the file is non-modifiable or readonly.
-              unnamed = "[No Name]", -- Text to show for unnamed buffers.
-            },
-          },
-        },
-        lualine_x = { "filetype" },
-        lualine_y = { "progress" },
-        -- lualine_z = {'location'}
-        lualine_z = {},
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {
-          {
-            "filename",
-            file_status = true, -- Displays file status (readonly status, modified status)
-            path = 1,           -- 0: Just the filename
-            -- 1: Relative path
-            -- 2: Absolute path
+						shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+						symbols = {
+							modified = "[+]", -- Text to show when the file is modified.
+							readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+							unnamed = "[No Name]", -- Text to show for unnamed buffers.
+						},
+					},
+				},
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				-- lualine_z = {'location'}
+				lualine_z = {},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- Displays file status (readonly status, modified status)
+						path = 1, -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
 
-            shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-            symbols = {
-              modified = "[+]",      -- Text to show when the file is modified.
-              readonly = "[-]",      -- Text to show when the file is non-modifiable or readonly.
-              unnamed = "[No Name]", -- Text to show for unnamed buffers.
-            },
-          },
-        },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = { "fugitive", "neo-tree", "fzf" },
-    },
-  },
+						shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+						symbols = {
+							modified = "[+]", -- Text to show when the file is modified.
+							readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+							unnamed = "[No Name]", -- Text to show for unnamed buffers.
+						},
+					},
+				},
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = { "fugitive", "neo-tree", "fzf" },
+		},
+	},
 
-  {
-    "nanozuki/tabby.nvim",
-    config = true,
-    lazy = false,
-    keys = {
-      { "<leader>tr", ":TabRename ", { silent = false } },
-    },
-  },
+	{
+		"nanozuki/tabby.nvim",
+		config = true,
+		lazy = false,
+		keys = {
+			{ "<leader>tr", ":TabRename ", { silent = false } },
+		},
+	},
 
-  {
-    "duff/vim-bufonly",
-    keys = {
-      { "<leader>co", "<cmd>BufOnly!<CR>" },
-    },
-  },
+	{
+		"duff/vim-bufonly",
+		keys = {
+			{ "<leader>co", "<cmd>BufOnly!<CR>" },
+		},
+	},
 
-  {
-    "qpkorr/vim-bufkill",
-    lazy = false,
-    keys = {
-      { "<C-x>", "<cmd>BW!<CR>" },
-    },
-  },
+	{
+		"qpkorr/vim-bufkill",
+		lazy = false,
+		keys = {
+			{ "<C-x>", "<cmd>BW!<CR>" },
+		},
+	},
 
-  { "romainl/vim-cool" },
+	{ "romainl/vim-cool" },
 
-  {
-    "tyru/open-browser.vim",
-    keys = {
-      { "gx", "<Plug>(openbrowser-open)",         mode = { "n" }, { silent = false } },
-      { "gx", "<Plug>(openbrowser-open)",         mode = { "v" }, { silent = false } },
-      { "gs", "<Plug>(openbrowser-smart-search)", mode = { "v" }, { silent = false } },
-    },
-  },
+	{
+		"tyru/open-browser.vim",
+		keys = {
+			{ "gx", "<Plug>(openbrowser-open)", mode = { "n" }, { silent = false } },
+			{ "gx", "<Plug>(openbrowser-open)", mode = { "v" }, { silent = false } },
+			{ "gs", "<Plug>(openbrowser-smart-search)", mode = { "v" }, { silent = false } },
+		},
+	},
 
-  {
-    "Keithbsmiley/investigate.vim",
-    config = function()
-      vim.g.investigate_use_dash = true
-    end,
-    lazy = false,
-  },
+	{
+		"Keithbsmiley/investigate.vim",
+		config = function()
+			vim.g.investigate_use_dash = true
+		end,
+		lazy = false,
+	},
 
-  {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	},
 
-  {
-    "stevearc/oil.nvim",
-    config = true,
-    opts = {
-      use_default_keymaps = false,
-      keymaps = {
-        ["g?"]    = "actions.show_help",
-        ["<CR>"]  = "actions.select",
-        ["<C-s>"] = "actions.select_vsplit",
-        ["<C-h>"] = "actions.select_split",
-        ["<C-t>"] = "actions.select_tab",
-        ["gp"]    = "actions.preview",
-        ["<C-c>"] = "actions.close",
-        ["q"]     = "actions.close",
-        ["<C-l>"] = "actions.refresh",
-        ["-"]     = "actions.parent",
-        ["_"]     = "actions.open_cwd",
-        ["`"]     = "actions.cd",
-        ["~"]     = "actions.tcd",
-        ["g."]    = "actions.toggle_hidden",
-      },
-    },
-    keys = {
-      { '-', '<cmd>Oil --float<CR>' },
-    }
-  },
+	{
+		"stevearc/oil.nvim",
+		config = true,
+		opts = {
+			use_default_keymaps = false,
+			keymaps = {
+				["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["<C-s>"] = "actions.select_vsplit",
+				["<C-h>"] = "actions.select_split",
+				["<C-t>"] = "actions.select_tab",
+				["gp"] = "actions.preview",
+				["<C-c>"] = "actions.close",
+				["q"] = "actions.close",
+				["<C-l>"] = "actions.refresh",
+				["-"] = "actions.parent",
+				["_"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = "actions.tcd",
+				["g."] = "actions.toggle_hidden",
+			},
+		},
+		keys = {
+			{ "-", "<cmd>Oil --float<CR>" },
+		},
+	},
 
-  {
-    'echasnovski/mini.jump',
-    version = '*',
-    config = true
-  },
-
-  {
-    'echasnovski/mini.jump2d',
-    version = false,
-    config = true,
-    opts = {
-      -- labels = 'abcdefghijklmnopqrstuvwxyz',
-      labels = 'asdfjkl;ghqweruioptyzxcv.,mnb',
-
-      view = {
-        -- Whether to dim lines with at least one jump spot
-        dim = false,
-
-        -- How many steps ahead to show. Set to big number to show all steps.
-        n_steps_ahead = 1,
-      },
-
-      allowed_windows = {
-        current = true,
-        not_current = false,
-      },
-    }
-  },
+	{
+		"smoka7/hop.nvim",
+		version = "*",
+		opts = {},
+		keys = {
+			{ "s", ":HopChar2<cr>", mode = { "n" }, { silent = true } },
+			{ "<space>w", ":HopWord<cr>", mode = { "n" }, { silent = true } },
+			{
+				"z",
+				function()
+					require("hop").hint_char1()
+				end,
+				mode = { "o", "v", "x", "s" },
+				{ remap = true, silent = false },
+			},
+		},
+	},
 }
