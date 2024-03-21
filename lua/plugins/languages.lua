52b95e812f9db52be1c3eb2a3ee2325bf7218ec6
Jacob Shafton <jacob@shafton.com>
2024-03-21 06:49:30 -0600

    language file formatting

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -1,22 +1,22 @@
 return {
-  { 'zimbatm/haproxy.vim' },
-  { 'stephpy/vim-yaml',               ft = 'yaml' },
-  { 'kchmck/vim-coffee-script',       ft = 'coffee' },
-  { 'slim-template/vim-slim',         ft = 'slim' },
-  { 'hashivim/vim-terraform',         ft = 'terraform' },
-  { 'towolf/vim-helm' },
-  { 'martinda/Jenkinsfile-vim-syntax' },
+	{ "zimbatm/haproxy.vim" },
+	{ "stephpy/vim-yaml", ft = "yaml" },
+	{ "kchmck/vim-coffee-script", ft = "coffee" },
+	{ "slim-template/vim-slim", ft = "slim" },
+	{ "hashivim/vim-terraform", ft = "terraform" },
+	{ "towolf/vim-helm", ft = "helm" },
+	{ "martinda/Jenkinsfile-vim-syntax" },
 
-  {
-    "cuducos/yaml.nvim",
-    ft = { "yaml" }, -- optional
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-		keys = {
-      { '<leader>yk', '<cmd>YAMLYankKey<CR>', mode = "n", { silent = false } }, -- yank yaml key
-      { '<leader>yv', '<cmd>YAMLYankKey<CR>', mode = "n", { silent = false } }, -- yank yaml value
+	{
+		"cuducos/yaml.nvim",
+		ft = { "yaml" }, -- optional
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- optional
 		},
-  }
+		keys = {
+			{ "<leader>yk", "<cmd>YAMLYankKey<CR>", mode = "n", { silent = false } }, -- yank yaml key
+			{ "<leader>yv", "<cmd>YAMLYankKey<CR>", mode = "n", { silent = false } }, -- yank yaml value
+		},
+	},
 }
