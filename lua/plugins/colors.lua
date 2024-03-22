12af2dd01fd5807506e63b796dcb3a6fdea00a0b
Jacob Shafton <jacob@shafton.com>
2024-03-22 07:06:29 -0600

    set comments in italic

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,19 @@
 return {
-  'EdenEast/nightfox.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme nordfox]])
-  end,
+	"EdenEast/nightfox.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	opts = {
+		options = {
+			transparent = true,
+			dim_inactive = true,
+			styles = {
+				comments = "italic",
+				functions = "bold",
+			},
+		},
+	},
+	init = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme nordfox]])
+	end,
 }
