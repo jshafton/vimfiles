5e8597db6c45f1fdcc7db9bd96fc2d8b9e20d95d
Jacob Shafton <jacob@shafton.com>
2024-06-17 08:40:11 -0600

    replace mini.files with yazi

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"mikavilpas/yazi.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		event = "VeryLazy",
+		keys = {
+			{
+				"-",
+				function()
+					require("yazi").yazi()
+				end,
+				desc = "Open the file manager",
+			},
+			{
+				-- Open in the current working directory
+				"<leader>cw",
+				function()
+					require("yazi").yazi(nil, vim.fn.getcwd())
+				end,
+				desc = "Open the file manager in nvim's working directory",
+			},
+		},
+		opts = {
+			open_for_directories = false,
+		},
+	},
+}
