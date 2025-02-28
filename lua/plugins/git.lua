6c7cd24a06ea36f37bd8047b53c621f37b005429
Jacob Shafton <jacob@shafton.com>
2025-02-28 08:39:36 -0700

    Consolidate some git functionality

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,24 +2,26 @@ return {
 	{
 		"tpope/vim-fugitive",
 		keys = {
-			-- { "<leader>gs", "<cmd>Git<CR>" },
+			{ "<leader>gs", "<cmd>Git<CR>" },
 			{ "<leader>gc", "<cmd>Git commit<CR>" },
 			{ "<leader>gca", "<cmd>Git commit --amend<CR>" },
 			{ "<leader>gb", "<cmd>Git blame<CR>" },
 			{ "<leader>gd", "<cmd>Gdiffsplit<CR>" },
 			{ "<leader>gr", "<cmd>Gread<CR>" },
-			{ "<leader>grm", "<cmd>Gread master:%<CR>" },
+			{ "<leader>grm", "<cmd>Gread $(git rev-parse --verify master || git rev-parse --verify main):%<CR>" },
 			{ "<leader>gw", "<cmd>Gwrite<CR>" },
 			{ "<leader>gp", "<cmd>Git push<CR>" },
 			{ "<leader>gpf", "<cmd>Git push --force<CR>" },
 			{ "<leader>gpsu", "<cmd>! git push -u origin $(git rev-parse --abbrev-ref HEAD)<CR>" },
 			{ "<leader>grb", "<cmd>! git pull --rebase<CR>" },
-			{ "<leader>ga", '<cmd>! git add .<CR> " adds everything to the index' },
-			{ "<leader>grh", '<cmd>! git reset .<CR> " git reset head -- unstages everything' },
+			{ "<leader>ga", "<cmd>! git add .<CR>" },
+			{ "<leader>grh", "<cmd>! git reset .<CR>" },
+
+			-- git 'pull request' to finish up a PR
 			{
 				"<leader>gpr",
 				"<cmd>! git push -u origin $(git rev-parse --abbrev-ref HEAD) && hub pull-request --no-edit -o<CR>",
-			}, -- git 'pull request' to finish up a PR
+			},
 		},
 	},
 
@@ -31,7 +33,7 @@ return {
 		dependencies = "nvim-lua/plenary.nvim",
 		opts = {
 			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
+				local gitsigns = require("gitsigns")
 
 				local function map(mode, l, r, opts)
 					opts = opts or {}
@@ -42,63 +44,61 @@ return {
 				-- Navigation
 				map("n", "]c", function()
 					if vim.wo.diff then
-						return "]c"
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
 					end
-					vim.schedule(function()
-						gs.next_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true })
+				end)
 
 				map("n", "[c", function()
 					if vim.wo.diff then
-						return "[c"
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
 					end
-					vim.schedule(function()
-						gs.prev_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true })
+				end)
 
 				-- Actions
-				map("n", "<leader>hs", gs.stage_hunk)
-				map("n", "<leader>hr", gs.reset_hunk)
-				map("v", "<leader>hs", function()
-					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				map("n", "<leader>ghs", gitsigns.stage_hunk)
+				map("n", "<leader>ghr", gitsigns.reset_hunk)
+
+				map("v", "<leader>ghs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end)
-				map("v", "<leader>hr", function()
-					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+
+				map("v", "<leader>ghr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 				end)
-				map("n", "<leader>hS", gs.stage_buffer)
-				map("n", "<leader>hu", gs.undo_stage_hunk)
-				map("n", "<leader>hR", gs.reset_buffer)
-				map("n", "<leader>hp", gs.preview_hunk)
-				map("n", "<leader>hb", function()
-					gs.blame_line({ full = true })
+
+				map("n", "<leader>ghp", gitsigns.preview_hunk)
+				map("n", "<leader>ghi", gitsigns.preview_hunk_inline)
+
+				map("n", "<space>g", function()
+					gitsigns.blame_line({ full = true })
 				end)
-				map("n", "<leader>tb", gs.toggle_current_line_blame)
-				map("n", "<leader>hd", gs.diffthis)
-				map("n", "<leader>hD", function()
-					gs.diffthis("~")
+
+				-- covered by fugitive
+				-- map("n", "<leader>hS", gitsigns.stage_buffer)
+				-- map("n", "<leader>hR", gitsigns.reset_buffer)
+				-- map("n", "<leader>ghd", gitsigns.diffthis)
+				-- map("n", "<leader>ghD", function()
+				-- 	gitsigns.diffthis("~")
+				-- end)
+
+				map("n", "<leader>ghQ", function()
+					gitsigns.setqflist("all")
 				end)
-				map("n", "<leader>td", gs.toggle_deleted)
+				map("n", "<leader>ghq", gitsigns.setqflist)
+
+				-- Toggles
+				map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+				map("n", "<leader>td", gitsigns.toggle_deleted)
+				map("n", "<leader>tw", gitsigns.toggle_word_diff)
 
 				-- Text object
-				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				map({ "o", "x" }, "ih", gitsigns.select_hunk)
 			end,
 		},
-	},
-
-	{
-		"rhysd/git-messenger.vim",
-		keys = {
-			{ "<space>g", "<cmd>GitMessenger<CR>" },
-		},
-		init = function()
-			vim.g.git_messenger_no_default_mappings = true
-			vim.g.git_messenger_include_diff = "curent"
-			vim.g.git_messenger_always_into_popup = true
-		end,
 	},
 
 	{
@@ -123,38 +123,7 @@ return {
 	{
 		"sindrets/diffview.nvim",
 		keys = {
-			{ "©", "<cmd>DiffviewOpen<CR>" },
-		},
-	},
-
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional
-		},
-		keys = {
-			{ "<leader>gZ", "<cmd>Neogit<CR>" },
-		},
-		opts = {
-			commit_editor = {
-				kind = "split",
-			},
-			integrations = {
-				-- If enabled, use telescope for menu selection rather than vim.ui.select.
-				-- Allows multi-select and some things that vim.ui.select doesn't.
-				telescope = nil,
-				-- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
-				-- The diffview integration enables the diff popup.
-				--
-				-- Requires you to have `sindrets/diffview.nvim` installed.
-				diffview = true,
-
-				-- If enabled, uses fzf-lua for menu selection. If the telescope integration
-				-- is also selected then telescope is used instead
-				-- Requires you to have `ibhagwan/fzf-lua` installed.
-				-- fzf_lua = true,
-			},
+			{ "©", "<cmd>DiffviewOlen<CR>" },
 		},
 	},
 }
