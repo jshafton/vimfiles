34eede1b39b4344a81dcb2b3caa561bc878ee9b3
Jacob Shafton <jacob@shafton.com>
2025-05-14 15:41:09 -0600

    Update lsp/mason config for nvim 0.11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,83 +17,92 @@ return {
           virtual_text = { spacing = 4, prefix = "●" },
           severity_sort = true,
         },
-        -- LSP Server Settings
-        servers = {
-          yamlls = {
-            settings = {
-              yaml = {
-                schemas = {
-                  ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.1-standalone/all.json"] = "*k8s**/*",
-                  ["https://raw.githubusercontent.com/ansible/schemas/main/f/ansible.json"] = "*ansible**/*",
-                  ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-                  ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                  ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-                  ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-                  ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-                  ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                  ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-                  ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-                  ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                  ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-                  ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-                },
-              },
-            },
-          },
-          lua_ls = {
-            settings = {
-              Lua = {
-                workspace = {
-                  checkThirdParty = false,
-                },
-                diagnostics = {
-                  -- Get the language server to recognize the `vim` global
-                  globals = { "vim", "use" },
-                },
-                completion = {
-                  callSnippet = "Replace",
-                },
-              },
-            },
-          },
-          helm_ls = {
-            yamlls = {
-              path = "yaml-language-server",
-            },
-          },
-          ruby_lsp = {
-            flags = {
-              debounce_text_changes = 150,
-            },
-            init_options = {
-              formatting = true,
-            },
-          },
-        },
       },
-      config = function(_, opts)
+      config = function(_, _)
         local lsp_config = require("lspconfig")
 
-        -- Ensure ruby-lsp's project root is set appropriately
-        opts.servers.ruby_lsp.root_dir = lsp_config.util.root_pattern("Gemfile", ".git")
+        vim.lsp.config["yamlls"] = {
+          settings = {
+            yaml = {
+              schemas = {
+                ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.1-standalone/all.json"] = "*k8s**/*",
+                ["https://raw.githubusercontent.com/ansible/schemas/main/f/ansible.json"] = "*ansible**/*",
+                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+              },
+            },
+          },
+        }
 
-        -- Set ruby_lsp to use asdf shim if present for project-local binaries
-        local ruby_lsp_cmd = os.getenv("HOME") .. "/.asdf/shims/ruby-lsp"
-        if vim.fn.filereadable(ruby_lsp_cmd) == 1 then
-          opts.servers.ruby_lsp.cmd = { ruby_lsp_cmd, "stdio" }
-        end
+        vim.lsp.config["lua_ls"] = {
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim", "use" },
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        }
+
+        vim.lsp.config["helm_ls"] = {
+          yamlls = {
+            path = "yaml-language-server",
+          },
+        }
+
+        vim.lsp.config["ruby-lsp"] = {
+          -- Set ruby_lsp to use asdf shim if present for project-local binaries
+          cmd = function()
+            local ruby_lsp_cmd = os.getenv("HOME") .. "/.asdf/shims/ruby-lsp"
+            if not vim.fn.filereadable(ruby_lsp_cmd) then
+              ruby_lsp_cmd = "ruby-lsp"
+            end
+            return { ruby_lsp_cmd, "stdio" }
+          end,
+
+          -- Ensure ruby-lsp's project root is set appropriately
+          root_dir = lsp_config.util.root_pattern("Gemfile", ".git"),
+
+          flags = {
+            debounce_text_changes = 150,
+          },
+          init_options = {
+            formatting = true,
+          },
+        }
 
         require("mason").setup()
-        require("mason-lspconfig").setup()
-
-        -- Ensure setup is called for every lsp server set up with Mason,
-        -- and ensure it's configured for (blink) completion
-        require("mason-lspconfig").setup_handlers({
-          function(server_name) -- default handler
-            local config = opts.servers[server_name] or {}
-            config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities or {})
-            lsp_config[server_name].setup(config)
-          end,
+        require("mason-lspconfig").setup({
+          ensure_installed = {
+            "bashls",
+            "lua_ls",
+            "denols",
+            "dockerls",
+            "groovyls",
+            "helm_ls",
+            "jedi_language_server",
+            "jsonls",
+            "pylsp",
+            "ruby_lsp",
+            "terraformls",
+            "yamlls",
+          },
         })
       end,
 
@@ -129,47 +138,19 @@ return {
       },
       ensure_installed = {
         "actionlint",
-        "bash-language-server",
         "coffeesense-language-server",
-        "deno",
-        "dockerfile-language-server",
         "flake8",
         "gitlint",
         "gitui",
-        "groovy-language-server",
         "hclfmt",
-        "helm-ls",
-        "jedi-language-server",
-        "json-lsp",
         "jsonlint",
-        "lua-language-server",
-        "python-lsp-server",
-        "ruby-lsp",
         "shellcheck",
         "shfmt",
         "sqlfmt",
         "stylua",
-        "terraform-ls",
         "tflint",
-        "typescript-language-server",
-        "yaml-language-server",
       },
     },
-    config = function(_, opts)
-      require("mason").setup(opts)
-
-      -- This is a custom implementation to ensure that all desired tools
-      -- are installed at startup, similar to a feature that's built in to
-      -- mason-lspconfig. It's a little better here because not everything is an
-      -- lsp server
-      local mr = require("mason-registry")
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end,
   },
 
   {
