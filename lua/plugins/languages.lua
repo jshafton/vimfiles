ed411e1f69c86f5224a6d0adb6564eb2325203d7
Jacob Shafton <jacob@shafton.com>
2025-05-26 07:21:15 -0600

    Add render-markdown plugin

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -30,4 +30,17 @@ return {
       -- NOTE: You must configure `cmd` in `config.cmd` unless you have installed via mason
     },
   },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    config = {
+      completions = { blink = { enabled = true } },
+    },
+  },
 }
