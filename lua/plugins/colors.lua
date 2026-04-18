036d33f6a44461b8994bf7812104c5c1683d3b29
Jacob Shafton <jacob@shafton.com>
2026-04-17 21:56:43 -0600

    switch to token colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,18 +1,25 @@
 return {
-	"EdenEast/nightfox.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	opts = {
-		options = {
-			transparent = true,
-			dim_inactive = true,
-			styles = {
-				functions = "bold",
-			},
-		},
-	},
-	init = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme nordfox]])
-	end,
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      options = {
+        transparent = true,
+        dim_inactive = true,
+        styles = {
+          functions = "bold",
+        },
+      },
+    },
+  },
+  {
+    "ThorstenRhau/token",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    init = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme token]])
+    end,
+  },
 }
